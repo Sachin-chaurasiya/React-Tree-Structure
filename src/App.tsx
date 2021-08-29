6d442a659bb1a51b842ae46acf187cbd342d8158
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-08-29 19:18:37 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Field from './components/Field/Field';
-import { JSONObject } from './data';
+import Field from "./components/Field/Field";
+import { JSONObject } from "./data";
 
-const {namespace,...newType}=JSONObject
+const { namespace, ...newType } = JSONObject;
 function App() {
   return (
     <div className="App">
--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,45 +1,64 @@
-import React,{useState,useCallback} from "react"
+import React, { useState, useCallback } from "react";
 import { getStyle } from "../../Utils/FieldUtils";
 
-import "./Field.css"
+import "./Field.css";
 
-type FieldProps={
-    name:string;
-    type:string;
-    fields?:Array<FieldProps>
-    isCollapsed?:boolean
-}
+type FieldProps = {
+  name: string;
+  type: string;
+  fields?: Array<FieldProps>;
+  isCollapsed?: boolean;
+};
 
-const Field = ({name,type,fields,isCollapsed=false}:FieldProps) => {
-    const [showChildren,setShowChildren]=useState<boolean>(isCollapsed);
-    const flag=(fields??[]).length>0
+const Field = ({ name, type, fields, isCollapsed = false }: FieldProps) => {
+  const [showChildren, setShowChildren] = useState<boolean>(isCollapsed);
+  const flag = (fields ?? []).length > 0;
 
-    const showChildrenHandler=useCallback(()=>{
-      setShowChildren(!showChildren)
-    },[showChildren,setShowChildren])
+  const showChildrenHandler = useCallback(() => {
+    setShowChildren(!showChildren);
+  }, [showChildren, setShowChildren]);
 
-    const getIcon=()=>{
-     return flag && (showChildren?<i className="fas fa-minus-circle"/>:<i className="fas fa-plus-circle"/>)
-    }
-
+  const getIcon = () => {
     return (
-        <div className="field-wrapper" style={{paddingLeft:flag?"26px":"0px"}}>
-            <div className="field-child" style={{marginLeft:flag?"-26px":"0px"}}>
-                <p className="field-child-icon" onClick={showChildrenHandler}>
-                {getIcon()}   
-                </p>   
-                <p className="field-label"  >
-                    <span style={{...getStyle(type),minWidth:"32px",textAlign:"center"}}>{type}</span>
-                    <span className="field-label-name">{name}</span>
-                </p>
-            </div>    
-            {flag && showChildren &&
-            <div className="child-fields-wrapper">
-            {(fields??[]).map((field,index)=><Field key={index} {...field} /> )} 
-            </div>
-            }
-        </div>
-    )
-}
+      flag &&
+      (showChildren ? (
+        <i className="fas fa-minus-circle" />
+      ) : (
+        <i className="fas fa-plus-circle" />
+      ))
+    );
+  };
 
-export default Field
+  return (
+    <div
+      className="field-wrapper"
+      style={{ paddingLeft: flag ? "26px" : "0px" }}
+    >
+      <div
+        className="field-child"
+        style={{ marginLeft: flag ? "-26px" : "0px" }}
+      >
+        <p className="field-child-icon" onClick={showChildrenHandler}>
+          {getIcon()}
+        </p>
+        <p className="field-label">
+          <span
+            style={{ ...getStyle(type), minWidth: "32px", textAlign: "center" }}
+          >
+            {type}
+          </span>
+          <span className="field-label-name">{name}</span>
+        </p>
+      </div>
+      {flag && showChildren && (
+        <div className="child-fields-wrapper">
+          {(fields ?? []).map((field, index) => (
+            <Field key={index} isCollapsed {...field} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Field;
